3c5be20ae8f26a703a0e93361a29f3a7fe3978f1
Federico Trimboli <federicotrimboli@gmail.com>
2018-08-11 17:38:27 -0300

    Ajustes

--- App/App/ViewController.swift
+++ App/App/ViewController.swift
@@ -11,17 +11,11 @@ import Models
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         let todo = Todo(id: nil, title: "Hola")
+        print(todo)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
