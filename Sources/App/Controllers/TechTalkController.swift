3adeeb8c41ad88e1de0a25a814b44c533bbff5d3
Federico Trimboli <federicotrimboli@gmail.com>
2018-08-13 10:43:47 -0300

    bla

--- Sources/App/Controllers/TechTalkController.swift
+++ Sources/App/Controllers/TechTalkController.swift
@@ -20,42 +20,20 @@ final class TechTalkController {
     }
 
     func index(_ req: Request) throws -> Future<[TechTalkDTO]> {
-        return TechTalk.query(on: req)
-            .join(\Review.techTalkID, to: \TechTalk.id, method: .left)
-            .alsoDecode(Review.self)
-            .all()
-            .map(to: [TechTalkDTO].self) { tuples in
-                let dictionary = try Dictionary(grouping: tuples) { tuple -> Int in
-                    guard let id = tuple.0.id else { throw Abort(.notFound) }
-                    return id
-                }
-                
-                return dictionary.map { arg in
-                    let (techTalkID, tuples) = arg
-                    let techTalk = tuples.first!.0
-                    return TechTalkDTO(id: techTalkID,
-                                       title: techTalk.title,
-                                       description: techTalk.description,
-                                       speaker: techTalk.speaker,
-                                       reviews: tuples.map { $0.1 })
+        return TechTalk.query(on: req).all().flatMap(to: [TechTalkDTO].self) { techTalks in
+            let futures = try techTalks.map { techTalk -> Future<TechTalkDTO> in
+                return try techTalk.reviews.query(on: req).all().map(to: TechTalkDTO.self) { reviews in
+                    let dto = TechTalkDTO(id: techTalk.id,
+                                          title: techTalk.title,
+                                          description: techTalk.description,
+                                          speaker: techTalk.speaker,
+                                          reviews: reviews)
+                    return dto
                 }
             }
-        
-        // Otra forma de conseguir lo mismo:
-//        return TechTalk.query(on: req).all().flatMap(to: [TechTalkDTO].self) { techTalks in
-//            let futures = try techTalks.map { techTalk -> Future<TechTalkDTO> in
-//                return try techTalk.reviews.query(on: req).all().map(to: TechTalkDTO.self) { reviews in
-//                    let dto = TechTalkDTO(id: techTalk.id,
-//                                          title: techTalk.title,
-//                                          description: techTalk.description,
-//                                          speaker: techTalk.speaker,
-//                                          reviews: reviews)
-//                    return dto
-//                }
-//            }
-//
-//            return futures.flatten(on: req)
-//        }
+
+            return futures.flatten(on: req)
+        }
     }
     
     func create(_ req: Request) throws -> Future<TechTalk> {
