8d71328eb5f459e81322902563d8723b13711701
Federico Trimboli <federicotrimboli@gmail.com>
2018-08-13 22:26:18 -0300

    fix

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,7 +11,7 @@ public func routes(_ router: Router) throws {
     let techTalkController = TechTalkController()
     router.get("techTalks", use: techTalkController.index)
     router.post("techTalks", use: techTalkController.create)
-    router.post("techTalks", TechTalk.parameter, "reviews", use: techTalkController.createReview)
+    router.post("techTalks", Int.parameter, "reviews", use: techTalkController.createReview)
     
     let speakerController = SpeakerController()
     router.post("speakers", use: speakerController.create)
